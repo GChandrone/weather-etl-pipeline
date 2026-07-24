f97a7ad922c1e39e185b7a90c5ef0c9ff2ffb74f
Gabriel da Cunha <gabriel.cunhagabi@gmail.com>
2026-07-24 18:56:30 -0300

    deploy: Pipeline ELT - Clima Joinville

---
+++ src/extract_data.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from pathlib import Path
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def extract_weather_data(url:str, params:dict) -> list:
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        logging.error("Erro na requisição")
+        return []
+    
+    data = response.json()
+    
+    if not data:
+        logging.warning("Nenhum dado retornado")
+        return []
+
+    output_path = 'data/weather_data.json'
+    output_dir = Path(output_path).parent
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    logging.info(f"Arquivo salvo em {output_path}")
+    return data
